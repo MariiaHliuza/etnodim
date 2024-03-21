39a1d860de654a4e737f73be716e7494e653820d
MariiaHliuza <156841784+MariiaHliuza@users.noreply.github.com>
2024-03-21 22:19:52 +0100

    main

---
+++ script.js
@@ -0,0 +1,6 @@
+function showOptions() {
+    document.querySelector('.hidden').style.display = 'block';
+  }
+  function hideOptions() {
+    document.querySelector('.hidden').style.display = 'none';
+  }
